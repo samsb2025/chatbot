0524517701ace469c055be009ffadca9c6b0606c
samsb2025 <samtil2000b@gmail.com>
2026-09-23 10:43:49 +0300

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export async function sendMessage(messages) {
   const response = await axios.post(
-    "http://localhost:5000/api/chat",
-
+    "https://simple-chatbot-backend-r0vj.onrender.com/api/chat",
     {
       messages,
     },
